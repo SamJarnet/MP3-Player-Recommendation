955c9997670664cd6bc22869e330d1d8c525e9aa
Sam Jarnet <jarnets17@holyportcollege.org.uk>
2023-11-26 23:36:22 +0000

    Lots of changes
    Added frame by frame checking for selected playlists, fixed song pausing and added playlist loading from text files

--- mp3.py
+++ mp3.py
@@ -12,7 +12,7 @@ class MP3Player:
         
 
         self.create_playlist_button = Button(self.master, text="Create Playlist", command=self.create_playlist)
-        self.create_playlist_button.place(x=50, y=65)
+        self.create_playlist_button.place(x=100, y=65)
         self.playlist_label = Label(self.master, text="Playlists")
         self.playlist_label.place(x=118, y=30)
         self.playlist_folder = Listbox(self.master, selectmode="SINGLE", bg="darkgrey", selectbackground="darkred", width=30, height=28)
@@ -24,8 +24,7 @@ class MP3Player:
         self.playlist.place(x=300, y=100)
         self.playlist_label = Label(self.master, text="No playlist selected")
         self.playlist_label.place(x=300, y=65)
-        self.select_playlist_button = Button(self.master, text="Select Playlist", command=self.select_playlist)
-        self.select_playlist_button.place(x=150, y=65)
+   
 
         self.recommended_label = Label(self.master, text="Recommended")
         self.recommended_label.place(x=900, y=230)
@@ -71,9 +70,10 @@ class MP3Player:
 
         self.stored_playlists = []
         self.stored_songs = []
-
+        
         self.selected_playlist = 0
-
+        self.currently_selected_playlist = ""
+        self.song = ""
         self.playlist_length = 0
 
         self.paused = False
@@ -82,19 +82,27 @@ class MP3Player:
         pygame.mixer.init()
 
 
+    def load_playlists(self):
+        txt = []
+        with open("playlists.txt", "r") as file:
+            txt = file.readlines()
+        for i in range(0, len(txt)):
+            self.stored_playlists.insert(0, txt[i])
+            self.playlist_folder.insert(0,  txt[i])
+            self.stored_songs.insert(0,(list(self.playlist_folder.get(0, -1))))
 
     def create_playlist(self):
         name = self.input_text.get(1.0, "end-1c")
         self.stored_playlists.insert(0, name)
         self.playlist_folder.insert(0, name)
         self.stored_songs.insert(0,(list(self.playlist_folder.get(0, -1))))
-        print(self.stored_songs)
-        with open('playlists.txt', "w") as file:
-            file.writelines(self.stored_playlists)
+        with open("playlists.txt", "w") as file:
+            for line in self.stored_playlists:
+                file.write("".join(line) + "\n")
 
 
     def select_playlist(self):
-        self.selected_playlist = self.playlist_folder.curselection()[0]
+        self.selected_playlist = self.currently_selected_playlist
         self.playlist_label.config(text = self.playlist_folder.get(self.selected_playlist))
         self.playlist.delete(0, "end")
         for i in range(0, len(self.stored_songs[self.selected_playlist])):
@@ -108,7 +116,7 @@ class MP3Player:
             # pygame.mixer.music.load(file_path)
             length = MP3(file_path).info.length
             self.stored_songs[self.selected_playlist].insert(0, os.path.basename(file_path))
-            print(self.stored_songs)
+           
             self.playlist_length += length
             self.statistics.delete(0, "end")
             self.statistics.insert(0, str(self.playlist_length)[0:6] + " seconds long")
@@ -118,20 +126,21 @@ class MP3Player:
 
 
     def play(self):
-        if self.paused == False or self.playlist.get(0):
+        if self.paused == False or self.song != self.playlist.get(self.playlist.curselection()[0]):
             pygame.mixer.music.load("C:/Users/hamue/Downloads/"+self.playlist.get(self.playlist.curselection()[0]))
+            self.song = self.playlist.get(self.playlist.curselection()[0])
             pygame.mixer.music.play()
-            self.currently_playing_song.config(text="Currently Playing: " + os.path.basename(self.playlist.get(0)))
-        # elif self.paused == True:
-        #     self.paused = False
-        #     pygame.mixer.music.unpause()
+            self.currently_playing_song.config(text="Currently Playing: " + os.path.basename(self.playlist.get(self.playlist.curselection()[0])))
+        elif self.paused == True:
+            self.paused = False
+            pygame.mixer.music.unpause()
         pause_image = PhotoImage(file="pause_button.png")
         self.play_button.config(image=pause_image, command=self.pause)
         self.play_button.image = pause_image
 
     def pause(self):
         pygame.mixer.music.pause()
-        #self.paused = True
+        self.paused = True
         play_image = PhotoImage(file="play_button.png")
         self.play_button.config(image=play_image, command=self.play)
         self.play_button.image = play_image
@@ -152,10 +161,23 @@ class MP3Player:
         pygame.mixer.music.set_volume(int(val) / 100)
 
 
-    
+    def check_selected_playlist(self):
+        if len(self.playlist_folder.curselection()) > 0:
+            self.currently_selected_playlist = self.playlist_folder.curselection()[0]
+            mp3_player.select_playlist()
+    def check_selected_song(self):
+        if len(self.playlist.curselection()) > 0:
+            pass
+
 
 if __name__ == "__main__":
     root = Tk()
     mp3_player = MP3Player(root)
-    root.mainloop()
+    mp3_player.load_playlists()
+    while True:
+        mp3_player.check_selected_playlist()
+        root.update_idletasks()
+        root.update()
+        
+        
     
