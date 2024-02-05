4876c7cea2cc4b86bd5cb36a057f7f7f8e01098c
Sam Jarnet <jarnets17@holyportcollege.org.uk>
2024-02-05 14:35:00 +0000

    wow

--- mp3.py
+++ mp3.py
@@ -3,7 +3,6 @@ import pygame
 from tkinter import Tk, Frame, Button, Label, Scale, Listbox, StringVar, PhotoImage, Text, Toplevel, Scrollbar, messagebox
 from tkinter import filedialog
 from mutagen.mp3 import MP3
-import random
 import datetime
 import numpy as np
 import pandas as pd
@@ -22,8 +21,6 @@ class MP3Player:
         self.master = master
         self.master.title("MP3 Player")
         self.master.geometry("1280x720")
-
-
 
 
         #creates and positions the button that is used to create a playlist
@@ -146,6 +143,10 @@ class MP3Player:
         self.account_button = Button(self.master, text="Accounts", command=self.create_login_table)
         self.account_button.place(x=1170, y=27)
 
+        #creates the queue button to open the queue frame
+        self.queue_button = Button(self.master, text="Queue", command=self.add_queue)
+        self.queue_button.place(x=950, y=625)
+
         #holds the playlists and songs that have been loaded
         self.stored_playlists = []
         self.stored_songs = []
@@ -177,13 +178,13 @@ class MP3Player:
         self.freeze = True
         self.paused = True
         self.playing = False
-
+        self.queue_open = False
 
         self.conn = sqlite3.connect("logins.db")
         self.cursor = self.conn.cursor()
         
         self.login_created = False
-
+        self.user = ""
 
         #initialises pygame and the pygame mixer
         pygame.init()
@@ -193,9 +194,10 @@ class MP3Player:
 
 
     #opens the playlists file and inserts all the playlists into the listbox and into the lists for storing
-    def initialise_playlists(self):
+    def initialise_playlists(self, path):
         txt = []
-        with open("playlists.txt", "r") as file:
+        self.playlist_folder.delete(0, "end")
+        with open("playlists" + path + ".txt", "r") as file:
             txt = file.readlines()
         for i in range(0, len(txt)):
             self.stored_playlists.insert(0, txt[i])
@@ -203,23 +205,24 @@ class MP3Player:
             self.stored_songs.insert(0,(list(self.playlist_folder.get(0, -1))))
             #insert an empty list to ensure that there is the correct number of indices for storing playlist lengths
     
+    
 
 
 
-
-    def initialise_songs(self):
+    def initialise_songs(self, path):
         for i in range(0, len(self.stored_playlists)):
-            if os.path.isfile(self.stored_playlists[i][0]+".txt"):
-                with open(self.stored_playlists[i][0]+".txt", "r") as file:
+            if os.path.isfile(self.stored_playlists[i][0]+ path +".txt"):
+                with open(self.stored_playlists[i][0]+ path +".txt", "r") as file:
                     txt=file.readlines()
                     for j in range(0, len(txt)):
                         self.stored_songs[i].append(txt[j])
             else:
-                with open(self.stored_playlists[i][0]+".txt", "w") as file:
+                with open(self.stored_playlists[i][0]+ path +".txt", "w") as file:
                     file.write("")
             self.time_played.append([0])
-            self.time_paused.append([0])    
+            self.time_paused.append([0])
  
+
     def remove_empty_lines(self, text_file):
         with open(text_file, "r") as file:
             new_arr = []
@@ -287,6 +290,19 @@ class MP3Player:
                     messagebox.showinfo("Success", "Logged in successfully.")
                     self.frame2.destroy()
                     self.login_created = False
+                    self.stored_playlists = []
+                    self.stored_songs = []
+                    self.user = username 
+                    self.playlist.delete(0, "end")
+                    try:
+                        mp3_player.initialise_playlists(username)
+                        mp3_player.initialise_songs(username)
+                    except:
+                        with open("playlists" + username + ".txt", "w") as file:
+                            file.write("")
+                        mp3_player.initialise_playlists(username)
+                        mp3_player.initialise_songs(username)
+
                 else:
                     messagebox.showerror("Error", "Invalid password.")
             else:
@@ -332,7 +348,6 @@ class MP3Player:
 
 
 
-
     def count_time(self):
         mp3_player.calculate_playlist_length()
         minutes = round(self.playlist_length // 60)
@@ -342,24 +357,15 @@ class MP3Player:
         else:
             hours = 0
         seconds = round(self.playlist_length % 60)
-        
-
         try:
-            
             mins = float(str(datetime.datetime.now())[-12:-10]) - float(str(self.start_time)[-12:-10]) - self.total_mins
             secs = float(str(datetime.datetime.now())[-9:-6]) - float(str(self.start_time)[-9:-6]) - self.total_secs
-
             if secs < 0:
                 secs+=60
                 mins-=1
-   
-                
         except:
             mins = 0
             secs = 0
-        
-
-
         self.statistics.insert(0, self.playlist_folder.get(self.selected_playlist) + " is " + str(hours) + " hours, " + str(minutes) + " minutes and " + str(seconds) + " seconds long")
         self.statistics.insert(1, datetime.datetime.now()-self.start_time)
         self.statistics.insert(2, str(self.total_mins) + " " + str(self.total_secs))
@@ -367,25 +373,21 @@ class MP3Player:
             self.statistics.insert(3, str(mins) + " minutes " + str(secs)  )
         else:
             self.statistics.insert(3, str(self.saved_mins) + " minutes " + str(self.saved_secs))
-
         self.statistics.delete(4, "end")
 
 
-
-
-       
            
-
 
     def create_playlist(self):
         name = self.input_text.get(1.0, "end-1c")
         self.stored_playlists.insert(0, name)
         self.playlist_folder.insert(0, name)
         self.stored_songs.insert(0,(list(self.playlist_folder.get(0, -1))))
-        with open("playlists.txt", "w") as file:
+        with open("playlists" + self.user + ".txt", "w") as file:
             for line in self.stored_playlists:
                 file.write("".join(line) + "\n")
-
+        with open(name[0] + self.user+  ".txt", "w") as file:
+            file.write("")
 
        
 
@@ -396,7 +398,7 @@ class MP3Player:
         with open("playlists.txt", "w") as file:
             for line in self.stored_playlists:
                 file.write("".join(line) + "\n")
-        mp3_player.remove_empty_lines("playlists.txt")
+        mp3_player.remove_empty_lines("playlists" + self.user + ".txt")
         self.playlist_folder.delete(0, "end")
         for i in range(0, len(self.stored_playlists)):
             self.playlist_folder.insert(0, self.stored_playlists[i])
@@ -406,20 +408,16 @@ class MP3Player:
 
 
 
-
-
     def delete_song(self):
         print(self.stored_songs)
         self.stored_songs[self.currently_selected_playlist].pop(len(self.stored_songs[self.currently_selected_playlist]) - self.playlist.curselection()[0] - 1)
-        with open(self.stored_playlists[self.currently_selected_playlist][0]+".txt", "w") as file:
+        with open(self.stored_playlists[self.currently_selected_playlist][0] + self.user +".txt", "w") as file:
             for line in self.stored_songs[self.currently_selected_playlist]:
                 file.write("".join(line) + "\n")
-        mp3_player.remove_empty_lines(self.stored_playlists[self.currently_selected_playlist][0]+".txt")
+        mp3_player.remove_empty_lines(self.stored_playlists[self.currently_selected_playlist][0] + self.user +".txt")
         self.playlist.delete(0, "end")
         for i in range(0, len(self.stored_songs[self.currently_selected_playlist])):
             self.playlist.insert(0, self.stored_songs[self.currently_selected_playlist][i])
-
-
 
 
 
@@ -442,12 +440,16 @@ class MP3Player:
         search_box = Text(master, height=1, width=10)
         search_box.place(x=200, y=20)
 
+
+
         def add_song():
             self.playlist.insert(0, load_box.get(load_box.curselection()[0]))
             self.stored_songs[self.selected_playlist].insert(0, load_box.get(load_box.curselection()[0]))
-            with open(self.stored_playlists[self.selected_playlist][0]+".txt", "w") as file:
+            with open(self.stored_playlists[self.selected_playlist][0]+ self.user +".txt", "w") as file:
                 for line in self.stored_songs[self.selected_playlist]:
                     file.write("".join(line) + "\n")
+
+
 
         def search_music():
             text = search_box.get(1.0, "end-1c")
@@ -466,30 +468,16 @@ class MP3Player:
         for i in range(0, 100):
             load_box.insert(0, data[14][i])
         
-        
-        
-
-
-
-
-        #SOUNDRAW
-
-    
-
-
 
 
     def select_playlist(self):
         self.selected_playlist = self.currently_selected_playlist
-        mp3_player.remove_empty_lines(self.playlist_folder.get(self.selected_playlist)[0]+".txt")
+        mp3_player.remove_empty_lines(self.playlist_folder.get(self.selected_playlist)[0]+ self.user + ".txt")
         self.playlist_label.config(text = self.playlist_folder.get(self.selected_playlist))
         self.playlist.delete(0, "end")
         for i in range(0, len(self.stored_songs[self.selected_playlist])):
             self.playlist.insert(0, self.stored_songs[self.selected_playlist][i])
         
-
-
-
 
 
 
@@ -500,11 +488,9 @@ class MP3Player:
             self.stored_songs[self.selected_playlist].insert(0, os.path.basename(file_path))
 
 
-            with open(self.stored_playlists[self.selected_playlist][0]+".txt", "w") as file:
+            with open(self.stored_playlists[self.selected_playlist][0]+ self.user +".txt", "w") as file:
                 for line in self.stored_songs[self.selected_playlist]:
                     file.write("".join(line) + "\n")
-
-
 
 
 
@@ -513,15 +499,14 @@ class MP3Player:
         self.recommended.insert(0, "Bosh")
 
 
+
     def add_selected_song(self):
         self.playlist.insert(0, self.recommended.get(self.recommended.curselection()[0]))
-        self.stored_songs[self.selected_playlist].insert(0, self.recommended.get(self.recommended.curselection()[0]))
+        self.stored_songs[self.selected_playlist].insert(0, self.recommendusered.get(self.recommended.curselection()[0]))
 
-        with open(self.stored_playlists[self.selected_playlist][0]+".txt", "w") as file:
+        with open(self.stored_playlists[self.selected_playlist][0]+ self.user +".txt", "w") as file:
             for line in self.stored_songs[self.selected_playlist]:
                 file.write("".join(line) + "\n")
-
-
 
 
 
@@ -585,21 +570,29 @@ class MP3Player:
         if self.saved_secs < 0:
             self.saved_secs+=60
             self.saved_mins-=1
-        
         self.freeze = True
 
+    def add_queue(self):
+        if self.queue_open == False:
+            self.queue_frame = customtkinter.CTkFrame(self.master, bg_color="lightgrey",fg_color="darkgrey", width=200, height=480, border_width=2, border_color="black")
+            self.queue_frame.place(x=640,y=100)
 
+            add_to_queue_button = customtkinter.CTkButton(self.queue_frame, text="Add to queue", command=self.add_to_queue)
+            add_to_queue_button.place(x=650, y=110)
+            self.queue_open = True
+        else:
+            self.queue_frame.destroy()
+            self.queue_open = False
 
-
+    def add_to_queue(self):
+        self.stored_songs
+        
 
     def skip_forward(self):
         pygame.mixer.music.stop()
         self.playlist.selection_clear(0, "end")
         self.playlist.selection_set((self.playlist.curselection()[0] + 1) % self.playlist.size())
         self.play()
-
-
-
 
 
 
@@ -610,15 +603,8 @@ class MP3Player:
         self.play()
 
 
-
-
-
-
     def set_volume(self, val):
         pygame.mixer.music.set_volume(int(val) / 100)
-
-
-
 
 
 
@@ -628,17 +614,14 @@ class MP3Player:
             mp3_player.select_playlist()
 
 
-    
-
-
 
 if __name__ == "__main__":
     root = Tk()
     mp3_player = MP3Player(root)
     #mp3_player.create_login_table()
     mp3_player.remove_empty_lines("playlists.txt")
-    mp3_player.initialise_playlists()
-    mp3_player.initialise_songs()
+    mp3_player.initialise_playlists("")
+    mp3_player.initialise_songs("")
     while True:
         mp3_player.check_selected_playlist()
         mp3_player.count_time()
